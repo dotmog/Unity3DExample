7eafc0af214e0320b68ab8aab40988c4555d467f
Cedric Decoster <cedric.decoster@gmail.com>
2020-10-01 00:09:13 +0200

    added MetaDataCrawler

--- Assets/Scripts/MetaDataCrawler.cs
+++ Assets/Scripts/MetaDataCrawler.cs
@@ -4,10 +4,23 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MetaDataCrawler : MonoBehaviour
 {
     private const string WEBSOCKETURL = "wss://boot.worldofmogwais.com";
+
+    private SubstrateClient _client;
+
+    public Image ImgConnect;
+
+    public Text TxtConnect;
+
+    public Text TxtButton;
+
+    public Text TxtMetaData;
+
+    private Task _awaitableTask;
 
     // Start is called before the first frame update
     void Start()
@@ -18,28 +31,49 @@ public class MetaDataCrawler : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (_awaitableTask != null && _awaitableTask.IsCompleted)
+        {
+            if (_client != null && _client.IsConnected)
+            {
+                ImgConnect.color = Color.green;
+                TxtConnect.text = "On";
+                TxtButton.text = "Disconnect";
+                TxtMetaData.text = _client.MetaData.Serialize();
+            } 
+            else
+            {
+                ImgConnect.color = Color.red;
+                TxtConnect.text = "Off";
+                TxtButton.text = "Connect";
+
+            }
+            
+            _awaitableTask = null;
+        }
     }
 
-    public void GetMetaDataClickedAsync()
+    private async Task ConnectAsync()
     {
-        var client = new SubstrateClient(new Uri(WEBSOCKETURL));
+        _client = new SubstrateClient(new Uri(WEBSOCKETURL));
+        await _client.ConnectAsync();
+    }
 
-        var connect = client.ConnectAsync();
-        //connect.Wait();
+    private async Task CloseAsync()
+    {
+        _client = new SubstrateClient(new Uri(WEBSOCKETURL));
+        await _client.CloseAsync();
+    }
 
-        //var systemName = client.System.NameAsync();
-        //systemName.Wait();
-
-        //var systemVersion = client.System.VersionAsync();
-        //systemVersion.Wait();
-
-        //var systemChain = client.System.ChainAsync();
-        //systemChain.Wait();
-
-        //Debug.Log($"Connected to System: {systemName} Chain: {systemChain} Version: {systemVersion}.");
-
-        //Debug.Log(client.MetaData.Serialize());
+    public void GetMetaDataClicked()
+    {
+        if (_client != null && _client.IsConnected)
+        {
+            _awaitableTask = CloseAsync();
+        }
+        else
+        {
+            _awaitableTask = ConnectAsync();
+        }
 
     }
 }
